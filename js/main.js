25f2f500ff68b550a7c2435f10a226b942913bcb
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-16 09:30:24 +0800

    Merge all js code in main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-// (function () {
-//
-var btnMobileMenu = document.body.querySelector(".mobile-menu-btn"),
-  mobileMenuWrapper = document.body.querySelector(".mobile-menu-wrapper");
+(function () {
+  var btnMobileMenu = document.body.querySelector(".mobile-menu-btn"),
+    mobileMenuWrapper = document.body.querySelector(".mobile-menu-wrapper"),
+    mainMenuWrapper = document.querySelector(".main-menu-wrapper"),
+    mainMenu = mainMenuWrapper.querySelector(".main-menu");
 
-if (btnMobileMenu) {
-  btnMobileMenu.addEventListener("click", function () {
-    mobileMenuWrapper.classList.toggle("show");
-    btnMobileMenu;
-  });
-}
-// })();
+  if (getComputedStyle(mainMenuWrapper).display !== "none") {
+    window.addEventListener("scroll", function () {
+      var topPos = mainMenuWrapper.getBoundingClientRect().top;
+      if (topPos <= 20) {
+        mainMenu.classList.add("sticked");
+      } else {
+        mainMenu.classList.remove("sticked");
+      }
+    });
+  }
+
+  if (btnMobileMenu) {
+    btnMobileMenu.addEventListener("click", function () {
+      mobileMenuWrapper.classList.toggle("show");
+      btnMobileMenu;
+    });
+  }
+})();
